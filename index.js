fb7f7ba41883273ebc21be9605ccc18d82da8518
apanacode <sammengi.sailokesh@gmail.com>
2023-11-27 19:48:30 +0530

    js code for date object

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var d1=new Date();
-document.writeln("<h2>");
-document.writeln("when their is no parameter : ",d1,"<br>");
+var d1=new Date()
+document.writeln("<h2>")
+document.writeln("when their is no parameter : ",d1,"<br>")
 var d2 =new Date(2000,9,15,22,23,445)
-document.writeln("when you entered the parameter : ",d2,"<br>");
+document.writeln("when you entered the parameter : ",d2,"<br>")
 var d3=new Date(98,0,4)
 document.writeln("when you entered only year, month, date :",d3,"<br>")
-var d4=new Date(20000);
-document.writeln(d4);
+var d4=new Date(20000)
+document.writeln(d4)
