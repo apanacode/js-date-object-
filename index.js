359a428307c3c9eb8562787a2ab4649c391cf049
apanacode <sammengi.sailokesh@gmail.com>
2023-11-28 22:25:11 +0530

    updated the date methods..

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ var d3=new Date(98,0,4)
 document.writeln("when you entered only year, month, date :",d3,"<br>")
 var d4=new Date(20000)
 document.writeln(d4)
+
+document.writeln("<b>METHODS FOR DATE OBJECT</b>","<BR>");
+var date=new Date();
+document.writeln("To get year method(getFullYear) : ",date.getFullYear(),"<br>");
+document.writeln("To get Month method(getMonth) : ",date.getMonth(),"<br>");
+document.writeln("To get date method(getDate) : ",date.getDate(),"<br>");
+document.writeln("To get hours method(getHours) : ",date.getHours(),"<br>");
+document.writeln("To get minutes method(getMinutes) : ",date.getMinutes(),"<br>");
+document.writeln("To get seconds method(getSeconds) : ",date.getSeconds(),"<br>");
