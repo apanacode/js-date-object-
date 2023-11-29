be7570d3b763c7e49fb25dbc05cb0404f38217a7
apanacode <sammengi.sailokesh@gmail.com>
2023-11-29 11:26:27 +0530

    implementation of getter and setter methods to current date

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ document.writeln("when you entered the parameter : ",d2,"<br>")
 var d3=new Date(98,0,4)
 document.writeln("when you entered only year, month, date :",d3,"<br>")
 var d4=new Date(20000)
-document.writeln(d4)
-
-document.writeln("<b>METHODS FOR DATE OBJECT</b>","<BR>");
+document.writeln(d4,"<br>");
+document.writeln("<br>")
+document.writeln("<b>GET METHODS FOR DATE OBJECT : </b>","<br>");
+document.writeln("<br>");
 var date=new Date();
 document.writeln("To get year method(getFullYear) : ",date.getFullYear(),"<br>");
 document.writeln("To get Month method(getMonth) : ",date.getMonth(),"<br>");
@@ -16,3 +17,22 @@ document.writeln("To get date method(getDate) : ",date.getDate(),"<br>");
 document.writeln("To get hours method(getHours) : ",date.getHours(),"<br>");
 document.writeln("To get minutes method(getMinutes) : ",date.getMinutes(),"<br>");
 document.writeln("To get seconds method(getSeconds) : ",date.getSeconds(),"<br>");
+document.writeln("<br>")
+document.writeln("<b>SET METHODS FOR DATE OBJECT : </b><br>");
+document.writeln("<br>");
+var currentdate=new Date();
+document.writeln("New date",currentdate,"<br>");
+currentdate.setFullYear(2023);
+document.writeln("set new year setFullYear() : ",currentdate,"<br>");
+currentdate.setMonth(3);
+document.writeln("set month",currentdate,"<br>")
+currentdate.setDate(22)
+document.writeln("set month : ",currentdate,"<br>");
+currentdate.setHours(12)
+document.writeln("set hours : ",currentdate,"<br>");
+currentdate.setMinutes(33);
+document.writeln("set minutes : ",currentdate,"<br>" );
+currentdate.setSeconds(45);
+document.writeln("set seconds : ",currentdate,"<br>");
+currentdate.setMilliseconds(333);
+document.writeln("set milliseconds : ",currentdate,"<br>");
